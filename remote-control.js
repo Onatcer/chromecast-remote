3dd439a814fbc034a91e88017658d1e39c628c58
Onatcer <me@onatcer.com>
2018-11-24 20:53:19 +0100

    added orf3 and orfs+

--- remote-control.js
+++ remote-control.js
@@ -35,6 +35,40 @@ function loadORF2() {
     }, 3000);
 }
 
+function loadORF3() {
+    if (!session) {
+        launchApp();
+    }
+    var waiter = setInterval(function () {
+        if (session) {
+            clearInterval(waiter);
+        }
+        loadMedia("https://orf3.cdn.ors.at/out/u/orf3/qxb/manifest_5.m3u8", "ORF III", "");
+    }, 3000);
+    document.querySelector("#orf3").classList = "loading";
+    setTimeout(function () {
+        clearactive();
+        document.querySelector("#orf3").classList = "active";
+    }, 3000);
+}
+
+function loadORFSP() {
+    if (!session) {
+        launchApp();
+    }
+    var waiter = setInterval(function () {
+        if (session) {
+            clearInterval(waiter);
+        }
+        loadMedia("https://orfs.cdn.ors.at/out/u/orfs/qxb/manifest_3.m3u8", "ORF Sport Plus", "");
+    }, 3000);
+    document.querySelector("#orfsp").classList = "loading";
+    setTimeout(function () {
+        clearactive();
+        document.querySelector("#orfsp").classList = "active";
+    }, 3000);
+}
+
 function clearactive() {
     var elems = document.querySelectorAll(".active");
 
